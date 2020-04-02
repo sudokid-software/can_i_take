732a43420c0ac21ee33162b2fd2d009c500fdd72
Emett Speer <speer.emett@gmail.com>
2020-04-01 21:33:41 -0700

    Started adding stuff

--- src/containers/Auth/Register/Register.js
+++ src/containers/Auth/Register/Register.js
@@ -1,25 +1,62 @@
 // import { Link } from "wouter";
-import React from "react";
+import React, { useRef } from "react";
 import Nav from "../../../components/NavBar/Navbar";
 
 const Register = () => {
+  const userNameInput = useRef(null);
+  const emailInput = useRef(null);
+  const password1Input = useRef(null);
+  const password2Input = useRef(null);
+  const form = useRef(null);
+  let errors = useRef(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const userName = userNameInput.current.value;
+    const email = emailInput.current.value;
+    const password1 = password1Input.current.value;
+    const password2 = password2Input.current.value;
+
+    if (
+      userName !== '' || email !== '' || password1 !== '' || password2 !== ''
+    ) {
+      errors = 'Please fill out the full form';
+      return;
+    }
+
+    if (password1 !== password2) {
+      errors = 'Please make sure that your passwords match';
+      return;
+    }
+
+    errors = false;
+    form.current.reset()
+  };
+
   return (
     <div>
       <Nav/>
       <div>
-        <form>
+        <form ref={form}>
+          {errors !== false?<span>{errors}</span>:<span></span>}
           <input type="text"
                  name="user_name"
-                 placeholder="User Name"/>
+                 placeholder="User Name"
+                 ref={userNameInput}/>
           <input type="text"
                  name="email"
-                 placeholder="Email"/>
+                 placeholder="Email"
+                 ref={emailInput}/>
           <input type="password"
                  name="password1"
-                 placeholder="Password"/>
+                 placeholder="Password"
+                 ref={password1Input}/>
           <input type="password"
                  name="password2"
-                 placeholder="Password"/>
+                 placeholder="Password"
+                 ref={password2Input}/>
+          <button onClick={handleClick}>Submit</button>
         </form>
       </div>
     </div>
