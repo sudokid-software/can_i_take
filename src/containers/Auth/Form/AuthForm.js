752704e81b92beeddcfdbfbe947025ed987d0385
Emett Speer <speer.emett@gmail.com>
2020-04-02 19:57:52 -0700

    Got registration form sending to backend

--- src/containers/Auth/Form/AuthForm.js
+++ src/containers/Auth/Form/AuthForm.js
@@ -1,16 +1,13 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import PropTypes from "prop-types";
-
+import axios from 'axios';
 import { reducer, init } from "./auth-form.reducer";
 
 import "./auth-form.scss";
 
 const AuthForm = props => {
   const [formData, setFormData] = useReducer(reducer, props, init);
-
-  const setError = (errString = "") => {
-    // console.error(errString);
-  };
+  const [error, setError] = useState('');
 
   const checkForErrors = () => {
     const { username, email, password1, password2 } = formData;
@@ -18,20 +15,24 @@ const AuthForm = props => {
     // Registration checks
     if (props.isRegister) {
       if (username === "" || email === "" || password1 === "" || password2 === "") {
-        return setError("Missing fields");
+        setError("Missing fields");
+        return true;
       }
 
       if (password1 !== password2) {
-        return setError("Please make sure that your passwords match");
+        setError("Please make sure that your passwords match");
+        return true;
       }
       // Login checks
     } else {
       if (username === "" || password1 === "") {
-        return setError("Missing fields");
+        setError("Missing fields");
+        return true;
       }
     }
 
     setError("");
+    return false;
   };
 
   const onChangeHandler = event => {
@@ -40,8 +41,9 @@ const AuthForm = props => {
 
   const onSubmitHandler = event => {
     event.preventDefault();
-
-    checkForErrors();
+    if (checkForErrors()) {
+      return;
+    }
 
     if (props.isRegister) {
       handleRegister();
@@ -50,28 +52,30 @@ const AuthForm = props => {
     }
   };
 
-  const handleRegister = () => {};
-
-  const handleLogin = () => {
-    fetch("/manifest.json")
-      .then(res => res.json())
-      .then(res => {
-        console.debug(res);
-
-        // document
-        if(res.statusText === 'OK'){
-          window.location.href = window.location.origin + "/main";
-        }
+  const handleRegister = () => {
+    const {username, email, password1, password2} = formData;
+    axios.post('http://localhost:4000/api/user/register', {
+      username,
+      email,
+      password1,
+      password2
+    })
+      .then(function (response) {
+        console.log(response);
       })
-      .catch(err => {
-        console.error(err);
+      .catch(function (error) {
+        console.log(error);
       });
   };
 
+  const handleLogin = () => {
+  };
+
   return (
-    <form className="auth-form">
+    <div className="auth-form">
       {props.heading ? <h1 className="auth-form--heading">{props.heading}</h1> : null}
       {/* Registration Email field */}
+      {error!==''?<span>{error}</span>:null}
       {props.isRegister ? (
         <input type="text" name="email" placeholder="Email" onChange={onChangeHandler} required />
       ) : null}
@@ -100,7 +104,7 @@ const AuthForm = props => {
         />
       ) : null}
       <button onClick={onSubmitHandler}>{props.isRegister ? "Register" : "Login"}</button>
-    </form>
+    </div>
   );
 };
 
